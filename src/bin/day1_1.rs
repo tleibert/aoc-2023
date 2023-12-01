cb6687cefd2757423f6dc65d890c8b3ed845bab9
Trevor Leibert <tleibert@salesforce.com>
2023-12-01 13:12:42 -0500

    Rewrite to not allocate/collect

--- src/bin/day1_1.rs
+++ src/bin/day1_1.rs
@@ -4,10 +4,11 @@ fn main() {
     let mut total = 0;
     for line in io::stdin().lines() {
         let line = line.unwrap();
-        let digits: Vec<_> = line.chars().filter_map(|c| c.to_digit(10)).collect();
+        let mut digits = line.chars().filter_map(|c| c.to_digit(10));
 
-        total += digits.first().unwrap_or(&0) * 10;
-        total += digits.last().unwrap_or(&0);
+        let first = digits.next();
+        total += first.unwrap_or(0) * 10;
+        total += digits.last().or(first).unwrap_or(0);
     }
 
     println!("Answer: {total}");
--- src/bin/day1_2.rs
+++ src/bin/day1_2.rs
@@ -8,22 +8,19 @@ fn main() {
     let mut total = 0;
     for line in io::stdin().lines() {
         let line = line.unwrap();
-        let digits: Vec<_> = line
-            .chars()
-            .enumerate()
-            .filter_map(|(idx, c)| {
-                c.to_digit(10).or_else(|| {
-                    DIGITS
-                        .iter()
-                        .enumerate()
-                        .find(|(_, &s)| line[idx..].starts_with(s))
-                        .map(|(idx, _)| idx as u32)
-                })
+        let mut digits = line.chars().enumerate().filter_map(|(idx, c)| {
+            c.to_digit(10).or_else(|| {
+                DIGITS
+                    .iter()
+                    .enumerate()
+                    .find(|(_, &s)| line[idx..].starts_with(s))
+                    .map(|(idx, _)| idx as u32)
             })
-            .collect();
+        });
 
-        total += digits.first().unwrap_or(&0) * 10;
-        total += digits.last().unwrap_or(&0);
+        let first = digits.next();
+        total += first.unwrap_or(0) * 10;
+        total += digits.last().or(first).unwrap_or(0);
     }
 
     println!("Answer: {total}");
